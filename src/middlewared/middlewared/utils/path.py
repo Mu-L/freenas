c11c9a0f1049dbd34c979ccc89cc9e31fde3a926
Waqar Ahmed <waqarahmedjoyia@live.com>
2020-07-17 04:15:55 +0500

    Introduce helper to retrieve module from file

--- src/middlewared/middlewared/utils/path.py
+++ src/middlewared/middlewared/utils/path.py
@@ -1,12 +1,21 @@
 # -*- coding=utf-8 -*-
+
+import importlib.util
 import logging
 import os
 
 logger = logging.getLogger(__name__)
 
-__all__ = ["is_child"]
+__all__ = ["is_child", "module_from_file"]
 
 
 def is_child(child: str, parent: str):
     rel = os.path.relpath(child, parent)
     return rel == "." or not rel.startswith("..")
+
+
+def module_from_file(module_name, file_path):
+    spec = importlib.util.spec_from_file_location(module_name, file_path)
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    return module
